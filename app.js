7b81be23ee2182c262785e134db95e863d9ffbfc
Daniil Borovov <superskillbro@gmail.com>
2023-08-31 18:57:02 +0600

    add game request

--- app.js
+++ app.js
@@ -20,6 +20,21 @@ app.get('/api/games', cors(corsOptions), async (req, res) => {
   }
 })
 
+app.get('/api/games/:id', cors(corsOptions), async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://www.freetogame.com/api/game?id=${req.params.id}`
+    )
+    const data = await response.json()
+    
+    res.json(data)
+    console.log(`Запрос на игру ${req.params.id} обработан`)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Internal server error' })
+  }
+})
+
 app.listen(3000, () => {
   console.log('Server started on port 3000')
 })
